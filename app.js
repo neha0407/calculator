304cc193f2212f801d046e28e1aa249c2b354660
neha0407 <48716678+neha0407@users.noreply.github.com>
2023-01-28 21:53:58 +0200

    added variables to js page

---
+++ app.js
@@ -0,0 +1,7 @@
+(function () {
+    let screen =document.querySelector('.screen');
+    let buttons =document.querySelector('.btn');
+    let clear =document.querySelector('.btn-clear');
+    let equal =document.querySelector('.btn-equal');
+   
+})()
